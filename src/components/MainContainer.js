6187fbbed9d38c2bac463260775a77517f04418d
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-01-25 19:41:34 +0530

    feat: add video background and title component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import VideoTitle from "./VideoTitle";
+import VideoBackground from "./VideoBackground";
 
 const MainContainer = () => {
-  return <div>MainContainer</div>;
+  const movies = useSelector((store) => {
+    return store.movies?.nowPlayingMovies;
+  });
+
+  if (!movies) {
+    return;
+  }
+
+  const mainMovie = movies[0];
+  console.log(mainMovie);
+
+  const { original_title, overview } = mainMovie;
+
+  return (
+    <div>
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground />
+    </div>
+  );
 };
 
 export default MainContainer;
---
+++ src/components/VideoTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoTitle = ({ title, overview }) => {
+  return <div>VideoTitle</div>;
+};
+
+export default VideoTitle;
