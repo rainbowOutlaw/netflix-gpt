ff943ce32e790187840658fed7d84c3bd1943cc6
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-01-26 10:18:09 +0530

    feat: add movie trailer as background and use react-redux

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
 
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+const VideoBackground = ({ movieId }) => {
+  const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
+  const dispatch = useDispatch();
+
+  const getMoviesVideos = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      API_OPTIONS
+    );
+    const json = await data.json();
+    // console.log(json);
+
+    const filter_data = json.results.filter((movie) => {
+      return movie.type === "Trailer";
+    });
+
+    const trailer = filter_data.length ? filter_data[0] : json.results[0];
+    console.log(trailer);
+
+    dispatch(addTrailerVideo(trailer));
+  };
+
+  React.useEffect(() => {
+    getMoviesVideos();
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=wCmv6oi5UB3l15qX`}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
